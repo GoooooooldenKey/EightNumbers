fb4d4ea60c60f3f3ec30c0801bf89009231b2ec5
Golden_key <golden.key@qq.com>
2021-10-02 12:43:48 +0800

    随便写了点功能, 但没用

--- EightNumbers/Logger.h
+++ EightNumbers/Logger.h
@@ -1,7 +1,17 @@
 #pragma once
+#include <string>
 #include <iostream>
+using std::string;
+using std::endl;
+using std::cout;
 
 class Logger {
-    
+    void print(string out) {
+        cout << out;
+    }
+
+    void printl(string out) {
+        cout << out << endl;
+    }
 };
 
